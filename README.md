bc0447257f3298fef9a959dc0ecd8cf4b0faff5e
Page <rickpagersc@gmail.com>
2016-08-03 18:17:23 -0400

    ignore sqlite and various minor updates

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
 RPCodes Django quickstart for Openshift
 
-Work in progress. Intended to ease the creation of a new Django project. Based off of 
-openshift/django-example
+Work in progress. 
+
+Intended to ease the creation of a new Django project, especially for REST API projects with user accounts. 
+Based off of openshift/django-example
 
 Combines common elements that I often use in Django projects:
 
-+Users
-+SessionAuth
++contrib.auth.Users
+++Registration
+++SessionAuth
+++Rate Limiting
 
 +Django Rest Framework
 ++Browsable API and JSON view
 ++Token Auth
+++Django-filters
 
 +Logging
 ++Logging the time it takes for a view to render (middleware)
--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -12,22 +12,10 @@ router.register(r'users', users.views.UserViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^api/', include(router.urls, namespace="api")),
-    url(r'^api-token-auth/', token_views.obtain_auth_token)
-]
+    
+    url(r'^$', include(router.urls, namespace="api")),
 
-# custom log in or out
-#from django.contrib.auth import views as rest_auth
-#
-#template_name = {'template_name': 'users/login.html'}
-#
-#urlpatterns += [
-#    url(r'^access/login/$', rest_auth.login, template_name, name='login'),
-#    url(r'^access/logout/$', rest_auth.logout, template_name, name='logout'),
-#]
-
-# specific to browseable API, though we could use this too if we like
-urlpatterns += [
+    url(r'^api-token-auth/', token_views.obtain_auth_token), 
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 ]
