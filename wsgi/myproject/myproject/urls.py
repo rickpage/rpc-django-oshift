976b3445988ef1caf8f4ce5be5967b6a97b70754
Page <rickpagersc@gmail.com>
2016-08-04 21:39:38 -0400

    add registration view

--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -6,16 +6,19 @@ from django.contrib import admin
 from rest_framework.authtoken import views as token_views
 from rest_framework import routers
 import users.views 
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'users', users.views.UserViewSet)
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name="register.html")),
     url(r'^admin/', include(admin.site.urls)),
     
-    url(r'^$', include(router.urls, namespace="api")),
+    url(r'^api/', include(router.urls, namespace="api")),
 
     url(r'^api-token-auth/', token_views.obtain_auth_token), 
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
+                
 ]
