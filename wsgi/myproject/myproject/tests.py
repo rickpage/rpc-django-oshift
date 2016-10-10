46a8fd10c41f8d03fc71eedb9148902daf1be91d
Page <rickpagersc@gmail.com>
2016-10-09 22:25:42 -0400

    test static files with requests module

--- wsgi/myproject/myproject/tests.py
+++ wsgi/myproject/myproject/tests.py
@@ -1,17 +1,32 @@
 from django.test import TestCase
 from django.test.client import Client
+from django.contrib.auth.models import User
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
-class StaticFilesTestCase(TestCase):
-    '''
-     django.test.client doesn't find static files on dev server
-    '''
-    test_url = "static/js.js"
-    
+import requests
+
+class RequestsTestCase(StaticLiveServerTestCase):
+    """
+    A base test case for Requests when running live server test case
+    """
+
     def setUp(self):
-        self.client = Client();
-        
-    def TODO_test_static_file_get(self):
-        '''
-        TODO: Check against a running sever
-        '''
         pass
+    def tearDown(self):
+        pass
+
+    def open(self, url):
+        return requests.get("%s%s" % (self.live_server_url, url))
+
+class StaticFilesTestCase(RequestsTestCase):
+    '''
+     django.test.client doesn't find static files when just using
+     TestCase and Client
+    '''
+    test_url = '/static/js/test.js'
+
+    def test_static_file_get(self):
+        r= self.open(self.test_url+'wrong')
+        self.assertTrue(r.status_code != 200)
+        r= self.open(self.test_url)
+        self.assertTrue(r.status_code == 200)
